91476ee86857bb0d1da0eec3ec7f9fc402749717
Danilo Oliveira <daniloautomica@gmail.com>
2024-11-05 23:16:48 -0300

    correção de tela e Banco de Dados

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,6 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-      //Chamar API
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=10",
-        {
-          method: "GET",
-        }
-      );
-
-      //Pegar os Dados que ela retorna
-      const data = await response.json();
-      //Armazenar/Persistir  esses dados no State
-      setTasks(data);
-    };
-    fetchTasks();
-  }, []);
-
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       //preciso atualizar esta tarefa
